7ae338a1642af95b964a02150efdb97e1d51c04f
nathaliemar <n.marten.95@gmail.com>
2025-06-27 15:59:08 +0200

    feat: Update project structure and dependencies; add static file serving

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 import { middlewareConfig } from "./config/middleware.config";
 import { errorHandler, notFoundHandler } from "./middleware/errorHandler";
+import path from "path";
 
 const PORT = process.env.PORT || 4000;
 dotenv.config();
@@ -17,6 +18,16 @@ app.use("/api", workoutRoutes);
 
 import sessionRoutes from "./routes/session.routes";
 app.use("/api", sessionRoutes);
+
+//STATIC FILES
+
+// Serve static files from Vue build
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Catch-all route to serve index.html (for Vue Router)
+app.get(/(.*)/, (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 //ERROR HANDLING
 app.use(notFoundHandler);
